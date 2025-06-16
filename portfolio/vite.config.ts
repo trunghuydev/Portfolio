e8995fecfebb2066394f8e0335d6bce3b7264b7d
Trung Huy <trunghuy832@gmail.com>
2025-06-16 16:59:52 +0700

    feature/fetch api get profile

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -6,7 +6,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src') // alias @ trỏ về thư mục src
-    }
-  }
-})
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [], 
+    },
+  },
+});
