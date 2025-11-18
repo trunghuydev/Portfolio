49bd7809edb884cf017eb5d0874f704c799d82c3
trunghuydev <trunghuy832@gmail.com>
2025-11-18 23:10:13 +0700

    a

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: [], 
+      external: [],
     },
   },
   test: {
